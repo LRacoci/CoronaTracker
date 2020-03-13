36c0156aebef81bbf393750aeea754d612143e2b
Lucas Racoci <racoci.0@gmail.com>
2020-03-13 13:16:24 -0300

    Fixing first recyclerview and starting the second one

--- app/src/main/java/com/github/coronatracker/ui/main/MainFragment.kt
+++ app/src/main/java/com/github/coronatracker/ui/main/MainFragment.kt
@@ -6,12 +6,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.github.coronatracker.R
-import com.xwray.groupie.ViewHolder
-import kotlinx.android.synthetic.main.brazil_item.*
-import kotlinx.android.synthetic.main.brazil_item.view.*
 import kotlinx.android.synthetic.main.main_fragment.*
+import kotlinx.android.synthetic.main.response.view.*
+import kotlinx.android.synthetic.main.value_response.*
+import kotlinx.android.synthetic.main.value_response.view.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
@@ -35,11 +35,20 @@ class MainFragment : Fragment(), CoroutineScope by MainScope() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         launch {
-            val brazilData = viewModel.getCoronaData().brazil
+            val data = viewModel.getCoronaData()
+            data.brazil.reverse()
             val context = context ?: return@launch
-            brazil_list.adapter = brazilData.adapt(context, R.layout.brazil_item) {
-                testShowItem.text = it.toString()
+            responses.apply {
+                layoutManager = LinearLayoutManager(activity)
+                adapter = data.brazil.adapt(context, R.layout.response) { response ->
+                    date.text = response.date
+                    time.text = response.time
+                    values_text.text = response.values.joinToString("\n") { value ->
+                           value.toString()
+                    }
+                }
             }
+
             message.text = viewModel.getCoronaData().toString()
         }
     }
--- app/src/main/java/com/github/coronatracker/ui/main/RecyclerViewSugar.kt
+++ app/src/main/java/com/github/coronatracker/ui/main/RecyclerViewSugar.kt
@@ -4,19 +4,25 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.LayoutRes
 import androidx.recyclerview.widget.RecyclerView
+import androidx.viewbinding.ViewBinding
 
 abstract class BindableViewHolder<T>(itemView: View) : RecyclerView.ViewHolder(itemView) {
     abstract fun bind(item: T): Unit
 }
 
-fun <T> List<T>.adapt(context: Context, itemId: Int, bindCall: View.(T) -> Unit) = object: RecyclerView.Adapter<BindableViewHolder<T>>() {
+fun <T> List<T>.adapt(
+    context: Context,
+    @LayoutRes itemId: Int,
+    bindCall: View.(T) -> Unit
+) = object: RecyclerView.Adapter<BindableViewHolder<T>>() {
     // Inflates the item views
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
     ): BindableViewHolder<T> {
-        val itemView = LayoutInflater.from(context).inflate(itemId, parent)
+        val itemView = LayoutInflater.from(context).inflate(itemId, parent, false)
         return object : BindableViewHolder<T>(itemView) {
             override fun bind(item: T) = itemView.bindCall(item)
         }
@@ -26,4 +32,24 @@ fun <T> List<T>.adapt(context: Context, itemId: Int, bindCall: View.(T) -> Unit)
     override fun onBindViewHolder(holder: BindableViewHolder<T>, position: Int) =
         holder.bind(get(position))
 
+}
+
+abstract class ViewBindViewHolder<V: ViewBinding, M>(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+fun <V: ViewBinding, M> List<M>.adapt(context: Context, bindCall: V.(M) -> Unit): RecyclerView.Adapter<ViewBindViewHolder<V, M>> {
+    return object : RecyclerView.Adapter<ViewBindViewHolder<V, M>>() {
+        override fun onCreateViewHolder(
+            parent: ViewGroup,
+            viewType: Int
+        ): ViewBindViewHolder<V, M> {
+            TODO("Not yet implemented")
+        }
+
+        override fun onBindViewHolder(holder: ViewBindViewHolder<V, M>, position: Int) {
+            get(position)
+        }
+
+        override fun getItemCount(): Int = size
+
+    }
 }
